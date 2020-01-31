04ab1b858d3f2cad0cdbb6c4c1b1719b0125a5dd
Lucas Santana <lucasg4x@gmail.com>
2020-01-31 14:45:35 -0300

    Aula 5.16 - Nomeando Rotas

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -35,18 +35,27 @@ Route::get('/seunome/{nome?}/{n}',function($nome=null,$n){
 
 })->where('nome','[A-za-z]+')->where('n','[0-9]+');
 
-Route::prefix('app')->group(function(){
+Route::prefix('aplicacao')->group(function(){
     Route::get('/',function(){
         return view('app');
-    });
+    })->name('app');
 
-    Route::get('/users',function(){
+    Route::get('/user',function(){
        return view('users');
-    });
+    })->name('app.user');
 
     Route::get('/profile',function(){
         return view('profile');
-    });
+    })->name('app.profile');
 
     
 });
+
+Route::get('/produtos',function(){
+    echo '<h1>Produtos</h1>
+            <ol>
+                <li>Notebook</li>
+                <li>Impressora</li>
+                <li>Mouse</li>
+            </ol>';
+})->name('meusprodutos');
