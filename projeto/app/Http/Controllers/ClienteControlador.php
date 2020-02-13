31bfe1d4fea22bc9c42e1724c5e48fd2abffc173
Lucas Santana <lucasg4x@gmail.com>
2020-02-13 00:16:42 -0300

    Aula 6.25 - Armazenando em sessão

--- projeto/app/Http/Controllers/ClienteControlador.php
+++ projeto/app/Http/Controllers/ClienteControlador.php
@@ -12,6 +12,14 @@ class ClienteControlador extends Controller
         ['id'=>3,'nome'=>'maria'],
         ['id'=>4,'nome'=>'aline']
     ];
+
+    public function __construct(){
+        $clientes = session('clientes');
+        if(!isset($clientes)){
+            session(['clientes'=>$this->clientes]);
+        }
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -19,7 +27,7 @@ class ClienteControlador extends Controller
      */
     public function index()
     {
-        $clientes = $this->clientes;
+        $clientes = session('clientes');
         return view('clientes.index', compact(['clientes']));
     }
 
@@ -42,13 +50,15 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
-        $id = count($this->clientes)+1;
+        $clientes = session('clientes');
+        $id = count($clientes) + 1;
         $nome = $request->nome;
-        $dados = ['id'=>$id,'nome'=>$nome];
-        $this->clientes[] = $dados;
+        $dados = ["id"=>$id,"nome"=>$nome];
+        $clientes[] = $dados;
+        session(['clientes'=>$clientes]);
 
         return redirect()->route('clientes.index');
-        
+
         /*As linhas abaixo são só para testar o salvamento dos dados*/
         /*$clientes = $this->clientes;
         return view('clientes.index', compact(['clientes']));*/
