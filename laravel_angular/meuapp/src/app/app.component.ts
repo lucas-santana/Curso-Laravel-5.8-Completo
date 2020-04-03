6377248fc92e0c7819e249a3ffb6ef482da6c271
Lucas Santana <lucasg4x@gmail.com>
2020-04-03 00:09:51 -0300

    Aula 209

--- laravel_angular/meuapp/src/app/app.component.ts
+++ laravel_angular/meuapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Post} from './post';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meuapp';
+
+  private posts: Post[]= [
+    new Post("João", "Meu post", "Sub joão", "joao@gmail.com","Minha msg"),
+    new Post("Paulo", "Post do Paulo", "Sub paulo", "paulo@gmail.com","Msg paulo"),
+    new Post("Maria", "Post da Maria", "Sub Maria", "maria@gmail.com","Msg da maria"),
+    new Post("João", "Meu post", "Sub joão", "joao@gmail.com","Minha msg"),
+    new Post("Paulo", "Post do Paulo", "Sub paulo", "paulo@gmail.com","Msg paulo"),
+    new Post("Maria", "Post da Maria", "Sub Maria", "maia@gmail.com","Msg da maria")
+  ]
 }
--- laravel_angular/meuapp/src/app/app.module.ts
+++ laravel_angular/meuapp/src/app/app.module.ts
@@ -20,10 +20,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
